527e66449cbf0476079bf6eadd0fcf066fdf8945
oleksandr <oleksandr@epunks.de>
2025-07-09 14:47:15 +0200

    add new bg and add range input

--- script.js
+++ script.js
@@ -1,63 +1,79 @@
 const grid = document.querySelector(".grid");
-const buttons = document.querySelectorAll(".options button");
 const clearButton = document.querySelector("#clearBtn");
+const randomBtn = document.querySelector("#randomBtn");
+
 
 const gridOptions = [
-    {
-        option: 16,
-        width: "32px",
-        height: "32px",
-        count: 256
-    },
-    {
-        option: 32,
-        width: "16px",
-        height: "16px",
-        count: 1024
-    },
-    {
-        option: 64,
-        width: "8px",
-        height: "8px",
-        count: 4096
-    }
-
+    { option: 16, width: "32px", height: "32px", count: 256 },
+    { option: 32, width: "16px", height: "16px", count: 1024 },
+    { option: 48, width: "10.66px", height: "10.66px", count: 2304 },
+    { option: 64, width: "8px", height: "8px", count: 4096 }
 ];
 
+let useRandomColor = false;
+
+randomBtn.addEventListener("click", () => {
+    useRandomColor = !useRandomColor;
+    randomBtn.textContent = useRandomColor ? "Black" : "Random color";
+});
+
 let isMouseDown = false;
+
+// track the mouse globally to enable draw painting
 document.body.addEventListener("mousedown", () => isMouseDown = true);
 document.body.addEventListener("mouseup", () => isMouseDown = false);
 
 
-function createGrid(option) {
+function generateColor() {
+    let hex = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += hex[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function createGrid(value) {
     grid.innerHTML = "";
 
-    const selectedOption = gridOptions.find(opt => opt.option == option);
+    const selectedOption = gridOptions.find(opt => opt.option === Number(value));
 
     if (!selectedOption) return;
 
     for (let i = 0; i < selectedOption.count; i++) {
-        const square = document.createElement("div");
-        square.style.width = selectedOption.width;
-        square.style.height = selectedOption.height;
+        const fragment = document.createElement("div");
+        fragment.style.width = selectedOption.width;
+        fragment.style.height = selectedOption.height;
 
-        square.addEventListener("mousedown", () => square.style.backgroundColor = "black");
-        
-        square.addEventListener("mousemove", () => {
+        fragment.addEventListener("mousedown", () => fragment.style.backgroundColor = useRandomColor ? generateColor() : "black");
+
+        fragment.addEventListener("mousemove", () => {
             if (isMouseDown) {
-                square.style.backgroundColor = "black"
+                fragment.style.backgroundColor = useRandomColor ? generateColor() : "black"
             }
         });
 
-        grid.appendChild(square);
+        grid.appendChild(fragment);
     }
 }
 
-buttons.forEach(button => button.addEventListener("click", () => createGrid(button.value)));
+const value = document.querySelector("#value");
+const input = document.querySelector("#input");
+value.textContent = input.value;
+input.addEventListener("input", (event) => {
+    value.textContent = event.target.value;
+    createGrid(input.value)
+});
 
 function clearGrid() {
     grid.innerHTML = "";
 }
 
-clearButton.addEventListener("click", () => clearGrid())
+// clearButton.addEventListener("click", () => clearGrid())
 
+
+document.addEventListener("DOMContentLoaded", () => {
+    input.value = 16;
+    value.textContent = "16";
+    createGrid(16);
+})
